81da75ab6a9cc11fc2ed9d170e4806e9e7d7adc0
FranciscoJoseKroff <franciscojose.kroff@alumnos.ulagos.cl>
2024-05-09 14:35:36 -0400

    Add files via upload

---
+++ digitos_numeros2.py
@@ -0,0 +1,10 @@
+num=int(input("Ingrese un numero"))
+if num<10:
+    print("El numero: ", num , "tiene un digito")
+if num>=10 and num<100:
+    print("el numero", num, "tiene dos digitos" )
+if num >= 100 and num<1000:
+    print("el numero", num, "tiene tres digitos")
+if num >=1000:
+    print("el numero", num , "tiene cuatro digitos")
+    
